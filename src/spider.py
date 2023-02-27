b22398bef8de4c6660267fa361c3c0975341d580
ChengXiang Qi <18630816527@163.com>
2023-02-27 10:20:33 +0800

    init

---
+++ src/spider.py
@@ -0,0 +1,86 @@
+import requests
+import datetime
+import random
+import re
+import os
+import sys
+
+current_date = datetime.date.today()
+
+def getHeaders():
+    user_agent_list = [
+    'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6',
+    'Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER',
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; 360SE)',
+    'Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20',
+    'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6',
+    'Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)',
+    'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12',
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E; LBBROWSER)',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.3 Mobile/14E277 Safari/603.1.30',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+    ]
+    headers = {
+        "User-Agent" : random.choice(user_agent_list),\
+    }
+    return headers
+
+
+def getData(url):
+    headers = getHeaders()
+    try:
+        response = requests.get(url=url, headers=headers)
+        if response.status_code == 200:
+            return response.json()["top_search"]["words"]
+        else: 
+            return None    
+    except ConnectionError as error: 
+        print("请求失败" + error)
+        return None
+
+
+def judge(file, question):
+    with open(file, mode='r', encoding='gbk') as f:
+        line = f.readline()
+        while line:
+            flag = line.find(question)
+            if flag != -1:
+                return False
+            line = f.readline()    
+    return True
+
+def trymkdir():
+    path = 'archives' + '/' + str(current_date)[0:7]
+    if not os.path.exists(path):
+        try:
+            os.makedirs(path)
+        except OSError as error:
+            print("error: "+ error)
+            sys.exit()
+
+
+def writeFile(data):
+    file = 'archives' + '/' + str(current_date)[0:7] + '/' + str(current_date)+'.md'
+    with open(file, mode='a+', encoding='gbk') as f:
+        for i in range(len(data)):
+            question = data[i]['display_query'].replace(' ','')
+            if judge(file, question):
+                f.write('[')
+                f.write(question)
+                f.write(']')
+                f.write('(')
+                f.write("https://www.zhihu.com/search?q="+str(question)+'&type=content')
+                f.write(')')
+                f.write('  \n')
+
